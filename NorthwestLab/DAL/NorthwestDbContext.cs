f2d7bc3be141f6b44bdc3f747396a230422c334d
John Martin <john.curtis.martin@hotmail.com>
2018-12-06 00:25:44 -0700

    Added DB Tables to DAL/NorthwestDBContext

--- NorthwestLab/DAL/NorthwestDbContext.cs
+++ NorthwestLab/DAL/NorthwestDbContext.cs
@@ -14,6 +14,39 @@ namespace NorthwestLab.DAL
 
         }
 
+        public DbSet<AssayResultTypes> AssayResultTypeTable { get; set; }
+        public DbSet<Assays> AssayTable { get; set; }
+        public DbSet<AssayTestTypes> AssayTestTypeTable { get; set; }
+        public DbSet<AssayTypes> AssayTypeTable { get; set; }
+        public DbSet<Compensation> CompensationTable { get; set; }
+        public DbSet<CompensationTypes> CompensationTypeTable{ get; set; }
+        public DbSet<CompoundReceipts> CompoundReceiptTable { get; set; }
+        public DbSet<Compounds> CompoundsTable { get; set; }
+        public DbSet<ConditionalDependencies> ConditionalDependencyTable { get; set; }
+        public DbSet<Customers> CustomerTable { get; set; }
+        public DbSet<DependencyTypes> DependencyTypeTable { get; set; }
+        public DbSet<Discounts> DicsountTable { get; set; }
+        public DbSet<DiscountTypes> DiscountTypeTable { get; set; }
+        public DbSet<Employees> EmployeeTable { get; set; }
+        public DbSet<Invoices> InvoiceTable { get; set; }
+        public DbSet<MaterialTypes> MaterialTypeTable { get; set; }
+        public DbSet<PaymentCards> PaymentCardTable { get; set; }
+        public DbSet<QualitativeResults> QualitativeResultTable { get; set; }
+        public DbSet<QuantitativeFileTypes> QuantitativeFileTypeTable { get; set; }
+        public DbSet<QuantitativeResults> QuantitativeResultTable { get; set; }
+        public DbSet<Samples> SampleTable { get; set; }
+        public DbSet<SampleTubes> SampleTubeTable { get; set; }
+        public DbSet<Schedule> ScheduleTable { get; set; }
+        public DbSet<State_Province> State_ProvinceTable { get; set; }
+        public DbSet<SummaryReports> SummaryReportTable { get; set; }
+        public DbSet<TestMaterialTypes> TestMaterialTypeTable { get; set; }
+        public DbSet<Tests> TestTable { get; set; }
+        public DbSet<TestTimeLog> TestTimeLogTable { get; set; }
+        public DbSet<TestTypeMaterials> TestTypeMaterialTable { get; set; }
+        public DbSet<TestTypes> TestTypeTable { get; set; }
+        public DbSet<Transactions> TransactionTable { get; set; }
+        public DbSet<WorkOrders> WorkOrderTable { get; set; }
+
     }
 
 }
